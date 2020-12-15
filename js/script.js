8ab6e3f3140c233660aa71e9856c9bdd78defe52
ichionse_hiroki <ichionse_hiroki@Yuta-Ichinose.local>
2020-12-16 00:26:24 +0900

    スライダーとフェードインができない

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 $(function () {
-    $("#nav").children("li").hover(function() {
-        $(this).children("ul").stop().slideToggle();
+    $(".parent-nav-content").hover(function() {
+        const index = $(".parent-nav-content").index(this);
+        $(".child-nav").eq(index).removeClass("active");
+    }, function() {
+        const index = $(".parent-nav-content").index(this);
+        $(".child-nav").eq(index).addClass("active");
     });
 
+    $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:5000,
+        dots:true,
+    });
+
+    $(window).scroll(function () {
+        $('.fadein').each(function() {
+            const elemPos = $(this).offset().top,
+                scroll = $(window).scrollTop(),
+                windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 100){
+                $(this).addClass('scrollin');
+            } else {
+                $(this).removeClass('scrollin');
+            }
+        });
+    });
 
 });
+
