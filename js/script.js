c9e8f9771d423ada9b827ede4e6897e5c9aab16f
ichionse_hiroki <ichionse_hiroki@Yuta-Ichinose.local>
2020-12-17 02:40:09 +0900

    スライダーとフェードインの実装

--- js/script.js
+++ js/script.js
@@ -1,30 +1,35 @@
 $(function () {
     $(".parent-nav-content").hover(function() {
         const index = $(".parent-nav-content").index(this);
-        $(".child-nav").eq(index).removeClass("active");
+        $(".child-nav").eq(index).removeClass("active").css("transform", "translate( 240px,0)");
+        // $(".child-nav").animate({"left" : "240px"}, 100);
     }, function() {
         const index = $(".parent-nav-content").index(this);
         $(".child-nav").eq(index).addClass("active");
     });
 
-    $('.slider').slick({
+    $(".slider").slick({
         autoplay:true,
         autoplaySpeed:5000,
         dots:true,
     });
 
+    
+    const pagetop = $('#page_top');   
+    pagetop.hide();
     $(window).scroll(function () {
-        $('.fadein').each(function() {
-            const elemPos = $(this).offset().top,
-                scroll = $(window).scrollTop(),
-                windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + 100){
-                $(this).addClass('scrollin');
-            } else {
-                $(this).removeClass('scrollin');
-            }
-        });
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
     });
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0}, 100);
+        return false;
+    });
+
 
 });
 
