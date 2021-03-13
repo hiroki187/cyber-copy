62e1b954a9b99f5943e3b2832c6ef0f41f6a9b4f
ichionse_hiroki <ichionse_hiroki@Yuta-Ichinose.local>
2021-03-13 20:30:17 +0900

    レスポンシブ途中

--- js/script.js
+++ js/script.js
@@ -13,13 +13,27 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 5000,
         dots: true,
+        arrow: false,
     });
     $(".p-top-pickup-slider").slick({
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0',
         dots: true,
-        slideToShow: 3,
-        slideToScroll: 1,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    centerPadding: '50px',
+                }
+            }
+        
+        ]
     });
 
     
